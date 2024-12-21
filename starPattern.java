e7c5e4039cc6de22f9b5c6028ca11699b9cdea43
avishkar <avishkarkurkute701@gmail.com>
2024-12-21 22:50:47 +0530

    corrected code

--- starPattern.java
+++ starPattern.java
@@ -4,7 +4,7 @@ public class starPattern {
     public static void main(String[] args) {
 
             for(int i = 5 ;i>0;i--){
-                for(int j = 0 ; j < 5; j++git add){
+                for(int j = 0 ; j < 5; j++){
                     System.out.print("* ");
             }System.out.println();
     }
