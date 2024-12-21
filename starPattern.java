94231b8f190acf545d9d19417ff50622dd2fa1a1
avishkar <avishkarkurkute701@gmail.com>
2024-12-21 22:56:23 +0530

    this i made on the pattern branch

--- starPattern.java
+++ starPattern.java
@@ -6,7 +6,7 @@ public class starPattern {
             for(int i = 5 ;i>0;i--){
                 for(int j = 0 ; j < 5; j++){
                     System.out.print("* ");
-            }System.out.println();
+            }System.out.println("This change I made for the new branch");
     }
     
 }
