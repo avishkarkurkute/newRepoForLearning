16d5fe5cb3197c7f4021d5d20036700902f0a989
avishkar <avishkarkurkute701@gmail.com>
2024-12-21 22:49:49 +0530

    square pattern

--- starPattern.java
+++ starPattern.java
@@ -3,10 +3,10 @@ public class starPattern {
 
     public static void main(String[] args) {
 
-            for(int i =0 ;i<5;i++){
-                for(int j = 0 ;j<=i;j++){
+            for(int i = 5 ;i>0;i--){
+                for(int j = 0 ; j < 5; j++git add){
                     System.out.print("* ");
-            }System.out.println("____________");
+            }System.out.println();
     }
     
 }
